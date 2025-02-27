7eb4e5ad1391d606ba3125747fcf4b3c899d4529
PiotrRomanczuk <77811265+PiotrRomanczuk@users.noreply.github.com>
2025-02-27 15:28:39 +0100

    Revert "WIP: calendar for apartment availability"
    This reverts commit 7483b583258800ed329d1db3c830d6bf24dfcd94.

--- src/app/calendar/LinksToFetch.ts
+++
@@ -1,6 +0,0 @@
-export const AirBnb_Link = new URL(
-	'https://www.airbnb.com/calendar/ical/921541253367297308.ics?s=3500d8b515272c463f72ae3529949d95&locale=pl'
-);
-export const Booking_Link = new URL(
-	'https://ical.booking.com/v1/export?t=be015c14-cbeb-44d5-b285-9a3e8928d745'
-);
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,18 +6,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	const res = NextResponse.next()
-
-    // add the CORS headers to the response
-    res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    res.headers.append(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-
-    return res,
 	matcher: [
 		/*
 		 * Match all request paths except for the ones starting with:
@@ -28,5 +16,4 @@ export const config = {
 		 */
 		'/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
 	],
-	
 };
